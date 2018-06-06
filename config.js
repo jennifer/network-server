62d6e92749c43050f867ccbe051516f748080482
jennifer kerssen <jjkerssen@gmail.com>
2018-06-05 20:20:21 -0600

    update travis.yml

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict'; 
 
-exports.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://localhost/site-bank';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/site-bank-test';
+exports.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://localhost/gallery';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/gallery-test';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
